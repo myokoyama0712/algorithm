be51881c95ab61c871614fa7793641545b931f06
Yokoyama Masahiro <masahiro.yokoyama@geniee.co.jp>
2018-09-25 18:26:59 +0900

    red state

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,6 +1,7 @@
 package tree
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,4 +25,22 @@ func TestAddChildNode(t *testing.T) {
 }
 
 func TestOutputNotEmptyNode(t *testing.T) {
+	node := NewNode(1)
+	node.AddChild(2)
+	node.AddChild(3)
+	node.children[0].AddChild(4)
+	node.children[0].AddChild(5)
+	node.children[1].AddChild(6)
+	node.children[1].AddChild(7)
+	node.children[1].AddChild(8)
+	expectedString := "id: 1, children: [2, 3]\n"
+	expectedString += "\tid: 2, children: [4, 5]\n"
+	expectedString += "\t\tid: 4, children: []\n"
+	expectedString += "\t\tid: 5, children: []\n"
+	expectedString += "\tid: 3, children: [6, 7, 8]\n"
+	expectedString += "\t\tid: 6, children: []\n"
+	expectedString += "\t\tid: 7, children: []\n"
+	expectedString += "\t\tid: 8, children: []\n"
+	fmt.Println(expectedString)
+	assert.Equal(t, expectedString, node.ToString())
 }
