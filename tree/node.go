47637a3c292a85ec74ca5460da310398937608e6
Yokoyama Masahiro <masahiro.yokoyama@geniee.co.jp>
2018-09-25 17:58:27 +0900

    green state

--- tree/node.go
+++ tree/node.go
@@ -1,17 +1,42 @@
 package tree
 
+import (
+	"fmt"
+)
+
+/*
+Node is struct denoting tree node.
+*/
 type Node struct {
 	id       int
-	children []Node
+	children []*Node
 }
 
+/*
+NewNode returns pointer of Node struct.
+*/
 func NewNode(id int) *Node {
 	node := new(Node)
 	node.id = id
-	node.children = []Node{}
+	node.children = []*Node{}
 	return node
 }
 
-func (n *Node) toString() string {
-	return "id: 1, children: []"
+/*
+ToString returns string formatted like
+"id: self, children: [child1, child2, ...]"
+*/
+func (n *Node) ToString() string {
+	return fmt.Sprintf("id: %d, children: %v", n.id, n.children)
+}
+
+/*
+AddChild append new child node to its childlen.
+*/
+func (n *Node) AddChild(id int) {
+	newChild := new(Node)
+	newChild.id = id
+	newChild.children = []*Node{}
+
+	n.children = append(n.children, newChild)
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -6,8 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOutputNodeStruct(t *testing.T) {
+func TestOutputEmptyNode(t *testing.T) {
 	id := 1
 	node := NewNode(id)
-	assert.Equal(t, "id: 1, children: []", node.toString())
+	assert.Equal(t, "id: 1, children: []", node.ToString())
+}
+
+func TestAddChildNode(t *testing.T) {
+	node := NewNode(1)
+	node.AddChild(2)
+	node.AddChild(3)
+	expectedSlice := []*Node{
+		&Node{id: 2, children: []*Node{}},
+		&Node{id: 3, children: []*Node{}},
+	}
+	assert.ElementsMatch(t, expectedSlice, node.children)
+}
+
+func TestOutputNotEmptyNode(t *testing.T) {
 }
